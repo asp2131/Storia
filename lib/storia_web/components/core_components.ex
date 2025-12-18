55469aaf5e7f2b7a72b9383637105012c195b91d
asp2131 <akinpds@gmail.com>
2025-12-18 14:00:35 -0600

    feat: enhance error component with customizable class attribute
    - Added a `class` attribute to the error message component for improved styling flexibility.
    - Updated the error message rendering to incorporate the new class attribute, allowing for dynamic class assignment.

--- lib/storia_web/components/core_components.ex
+++ lib/storia_web/components/core_components.ex
@@ -405,11 +405,12 @@ defmodule StoriaWeb.CoreComponents do
   @doc """
   Generates a generic error message.
   """
+  attr :class, :string, default: nil
   slot :inner_block, required: true
 
   def error(assigns) do
     ~H"""
-    <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600">
+    <p class={["mt-3 flex gap-3 text-sm leading-6 text-rose-600", @class]}>
       <.icon name="hero-exclamation-circle-mini" class="mt-0.5 h-5 w-5 flex-none" />
       <%= render_slot(@inner_block) %>
     </p>
